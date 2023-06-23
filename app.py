c96c0c7d8b3f7aa56e655328e56b870f72c75d70
whyhawk <135528494+whyhawk@users.noreply.github.com>
2023-06-23 21:56:18 +0800

    complete change from web.py to flask
    complete change from web.py to flask, in order to automatically deploy into Zeabur

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-import web
+from flask import Flask, render_template, request
+import os
 
-render = web.template.render('templates/')  # 设置模板文件夹路径
+app = Flask(__name__)
 
-urls = (
-    '/', 'Index',
-)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        input_text = request.form['input_text']  # 获取文本框中的输入内容
+        processed_text = process_input(input_text)  # 处理输入内容
+        return render_template('index.html', result=processed_text)
+    return render_template('index.html')
 
-class Index:
-    def GET(self):
-        return render.index(result=None)
+def process_input(input_text):
+    # 在这里进行处理输入内容的逻辑
+    processed_text = input_text
+    return processed_text
 
-    def POST(self):
-        input_text = web.input().text  # 获取表单数据内容
-        return render.index(result=input_text)
-
-if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True,port=os.getenv("PORT", default=5000),host='0.0.0.0')
